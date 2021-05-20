efb59f9f2aa17bea1fa131827a9447dc45bbea40
sukeshnibakshi29 <82715100+sukeshnibakshi29@users.noreply.github.com>
2021-05-19 18:08:29 -1200

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,40 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          'restitution': 0.4,
+          'friction' : 1.0,
+           }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      this.image = loadImage("block.png");
+      this.visibility = 255
+    }
+    display(){
+
+      if(this.body.speed<3) {
+        var pos= this.body.position;
+       var angle= this.body.angle;
+   push ()
+   translate (pos.x,pos.y);
+   rectMode(CENTER);
+   rotate(angle);
+   rect(0,0,this.width, this.height);
+   pop ()
+      }
+   else{
+   World.remove(world, this.body);
+   push();
+      this.visibility = this.visibility -5
+      tint(255,this.visibility);
+      rect(1000,1000,this.width, this.height);
+    pop()
+      
+   }
+     
+     
+  
+}
+}
+
